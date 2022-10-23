c5ab106906aa9c5a41aaca32be58160344061d01
Muhammadazizprogromer <103125035+Muhammadazizprogromer@users.noreply.github.com>
2022-10-23 14:22:38 +0500

    heroli profile

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--6507!w078l1he!s$%abqq*t)dl*#wu#9asw56v+n6ees)*0dn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
